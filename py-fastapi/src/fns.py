029abbff3577ba0a6f0bdf86a0e79c873b4c79f5
Aaron Harper <aaron@inngest.com>
2025-07-01 10:16:54 -0400

    Make FastAPI example async

--- py-fastapi/src/fns.py
+++ py-fastapi/src/fns.py
@@ -7,5 +7,5 @@ from .client import inngest_client
     fn_id="fn-1",
     trigger=inngest.TriggerEvent(event="event-1"),
 )
-def fn_1(ctx: inngest.ContextSync) -> str:
+async def fn_1(ctx: inngest.Context) -> str:
     return "Hello world!"
