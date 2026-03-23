736316057f7b5026a4549508fc48b1055efb6411
Aaron Harper <aaron@inngest.com>
2026-03-23 16:02:19 -0400

    Fix INNGEST_DEV

--- py-fastapi/src/fns.py
+++ py-fastapi/src/fns.py
@@ -3,30 +3,9 @@ import inngest
 from .client import inngest_client
 
 
-async def do_a() -> None:
-    return 1
-
-
-async def do_b() -> None:
-    return 2
-
-
-async def do_c() -> None:
-    pass
-
-
 @inngest_client.create_function(
     fn_id="fn-1",
-    # trigger=inngest.TriggerEvent(event="event-1"),
-    trigger=[
-        inngest.TriggerEvent(event="general"),
-        inngest.TriggerEvent(
-            event="event-1",
-            expression="event.data.value > 10",
-        ),
-    ],
+    trigger=inngest.TriggerEvent(event="event-1"),
 )
 async def fn_1(ctx: inngest.Context) -> str:
-    print("triggered", ctx.event.id)
-
-    return "fn"
+    return "Hello world!"
